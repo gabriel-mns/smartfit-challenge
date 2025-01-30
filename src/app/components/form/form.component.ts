7961ff79a4a8b5b753b7f2195d950bb90f57dd94
Gabriel Martins <gabrielgmns071204@gmail.com>
2025-01-29 21:27:18 -0300

    feat: adicionado estrutura do formulário e estilização

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-form',
+    imports: [],
+    templateUrl: './form.component.html',
+    styleUrl: './form.component.scss'
+})
+export class FormComponent {
+
+    resultados = [];
+
+}
