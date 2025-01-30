0bc06964f7ff0bd07a36cbafce69ac287e5ddef7
Gabriel Martins <gabrielgmns071204@gmail.com>
2025-01-29 21:51:18 -0300

    feat: adicionada estrutura básica das funções do formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FormComponent } from "./components/form/form.component";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-form',
-    imports: [],
+    imports: [ReactiveFormsModule],
     templateUrl: './form.component.html',
     styleUrl: './form.component.scss'
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
 
     resultados = [];
+    formularioAcademias !: FormGroup;
+
+    constructor(private formBuilder:FormBuilder){ }
+
+    ngOnInit(): void {
+        
+        this.formularioAcademias = this.formBuilder.group({
+            periodo: '',
+            mostrarFechadas: false
+        })
+
+    }
+
+    onSubmit(){
+
+        console.log("submit");
+
+    }
+
+    onClean(){
+
+        this.formularioAcademias.reset();
+
+    }
+
 
 }
