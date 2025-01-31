6e2e4ceb72a23389e72510d7261d262bc4955c79
Gabriel Martins <gabrielgmns071204@gmail.com>
2025-01-31 16:18:09 -0300

    feat: adicioando exibição responsiva de unidades em lista de cartões

--- src/app/components/cartao/cartao.component.ts
+++ src/app/components/cartao/cartao.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Unidade } from '../../types/unidade.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cartao',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cartao.component.html',
   styleUrl: './cartao.component.scss'
 })
 export class CartaoComponent {
 
-  // @Input() unidade !: Unidade;
+  @Input() unidade !: Unidade;
 
 }
--- src/app/components/lista-cartoes/lista-cartoes.component.ts
+++ src/app/components/lista-cartoes/lista-cartoes.component.ts
@@ -3,6 +3,7 @@ import { UnidadeService } from '../../services/busca-unidades.service';
 import { Unidade } from '../../types/unidade.interface';
 import { CartaoComponent } from "../cartao/cartao.component";
 
+
 @Component({
   selector: 'app-lista-cartoes',
   imports: [CartaoComponent],
@@ -12,6 +13,61 @@ import { CartaoComponent } from "../cartao/cartao.component";
 export class ListaCartoesComponent implements OnInit {
 
   @Input() listaUnidades:Unidade[] = [];
+
+  legendas = [
+    {
+      titulo: 'Máscara',
+      restricoes:[
+        {
+          tipo: 'Obrigatório',
+          icone: '/required-mask.png'
+        },
+        {
+          tipo: 'Recomendado',
+          icone: '/recommended-mask.png'
+        }
+      ]
+    },
+    {
+      titulo: 'Toalha',
+      restricoes:[
+        {
+          tipo: 'Obrigatório',
+          icone: '/required-towel.png'
+        },
+        {
+          tipo: 'Recomendado',
+          icone: '/recommended-towel.png'
+        }
+      ]
+    },
+    {
+      titulo: 'Bebedouro',
+      restricoes:[
+        {
+          tipo: 'Parcial',
+          icone: '/partial-fountain.png'
+        },
+        {
+          tipo: 'Proibido',
+          icone: '/not_allowed-fountain.png'
+        }
+      ]
+    },
+    {
+      titulo: 'Vestiários',
+      restricoes:[
+        {
+          tipo: 'Liberado',
+          icone: '/allowed-lockerroom.png'
+        },
+        {
+          tipo: 'Proibido',
+          icone: '/closed-lockerroom.png'
+        }
+      ]
+    }
+  ]
 
   constructor() { }
 
